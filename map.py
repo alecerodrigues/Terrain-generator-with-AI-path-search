fceb5faa41eed3302074ffe3537f9517b32118b9
aer153 <alec.rodrigeus@rutgers.edu>
2020-10-12 22:22:43 -0400

    + Map size fixed + Menu implementation + Map Info saving to file + Bug fixes

--- map.py
+++ map.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import math
+import functools
+import operator
 
-curr_map = np.empty((160, 120))
+curr_map = np.empty((120, 160))
 rough_terrain_centers = None
 highways = None
 
@@ -16,13 +19,13 @@ highways = None
 
 def rand_x():
     """ Returns a value within the x domain - 0 to 160"""
-    x = random.randint(0, 159)
+    x = random.randint(0, 119)
     return x
 
 
 def rand_y():
     """ Returns a value within the y domain - 0 to 120"""
-    y = random.randint(0, 119)
+    y = random.randint(0, 159)
     return y
 
 
@@ -41,6 +44,21 @@ def rand_sixty_forty():
         return 0
 
 
+def save_map(map, s_start, s_goal, rtc, file_name):
+    f = open(file_name, "w")
+    start_p = '(' + str(s_start[0]) + ', ' + str(s_start[1]) + ')'
+    end_p = '(' + str(s_goal[0]) + ', ' + str(s_goal[1]) + ')'
+    f.write(start_p + '\n')
+    f.write(end_p + '\n')
+    for i in rtc:
+        p = '(' + str(i[0]) + ', ' + str(i[1]) + ')'
+        f.write(p + '\n')
+    for x in range(120):
+        for y in range(160):
+            f.write(str(int(map.item((x, y)))) + " ")
+        f.write('\n')
+
+
 def highway_gen():
     all_highway_paths = []
     for k in range(4):
@@ -54,7 +72,7 @@ def highway_gen():
             current_highway_path.append(starting_point)
             next_x_val = 1
             next_y_val = y_val
-            while next_x_val != 160:
+            while next_x_val != 120:
                 if rand_sixty_forty() == 1:
                     next_point = [next_x_val, next_y_val]
                     current_highway_path.append(next_point)
@@ -63,7 +81,7 @@ def highway_gen():
                 elif rand_sixty_forty() == 0:
                     coin = rand_coin_flip()
                     if coin == 0:
-                        if next_y_val < 120:
+                        if next_y_val < 160:
                             next_y_val += 1
                         next_point = [next_x_val, next_y_val]
                         current_highway_path.append(next_point)
@@ -82,7 +100,7 @@ def highway_gen():
             current_highway_path.append(starting_point)
             next_y_val = 1
             next_x_val = x_val
-            while next_y_val != 120:
+            while next_y_val != 160:
                 if rand_sixty_forty() == 1:
                     next_point = [next_x_val, next_y_val]
                     current_highway_path.append(next_point)
@@ -128,41 +146,201 @@ def block_cell_gen():
     for k in range(3840):
         x_val = rand_x()
         y_val = rand_y()
+        point = [x_val, y_val]
         bool_val = 0
         for h in highways:
             for i in h:
                 if x_val == i[0] and y_val == i[1]:
                     bool_val = 1
 
-        if bool_val == 1:
+        if bool_val == 1 or point in block_cells:
             k -= 1
         elif bool_val == 0:
-            point = [x_val, y_val]
+
             block_cells.append(point)
     return block_cells
 
 
+def generate_h(start_point, end_point, map):
+    sp_cell_type = map.item(start_point[0], start_point[1])
+    ep_cell_type = map.item(end_point[0], end_point[1])
+
+    if sp_cell_type == 0 or ep_cell_type == 0:
+        return -1
+
+    # UnB to UnB
+    if sp_cell_type == 1 and ep_cell_type == 1:
+        # Diag Check
+        if (start_point[0] + 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] - 1 == end_point[1]):
+            return math.sqrt(2)
+        # V & H Check
+        elif (start_point[0] == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] == end_point[1]):
+            return 1
+
+    # R to R
+    if sp_cell_type == 2 and ep_cell_type == 2:
+        # Diag Check
+        if (start_point[0] + 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] - 1 == end_point[1]):
+            return math.sqrt(8)
+        # V & H Check
+        elif (start_point[0] == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] == end_point[1]):
+            return 2
+
+    # UnB to R
+    if (sp_cell_type == 1 and ep_cell_type == 2) or (sp_cell_type == 2 and ep_cell_type == 1):
+        # Diag Check
+        if (start_point[0] + 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] - 1 == end_point[1]):
+            return (math.sqrt(2) + math.sqrt(8)) / 2
+        # V & H Check
+        elif (start_point[0] == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] == end_point[1]):
+            return 1.5
+
+    # UnB-HW to UnB-HW
+    if sp_cell_type == 3 and ep_cell_type == 3:
+        # Diag Check
+        if (start_point[0] + 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] - 1 == end_point[1]):
+            return math.sqrt(2) / 4
+        # V & H Check
+        elif (start_point[0] == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] == end_point[1]):
+            return 1 / 4
+
+    # Rough-HW to Rough-HW
+    if sp_cell_type == 4 and ep_cell_type == 4:
+        # Diag Check
+        if (start_point[0] + 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] - 1 == end_point[1]):
+            return math.sqrt(8) / 4
+        # V & H Check
+        elif (start_point[0] == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] == end_point[1]):
+            return 2 / 4
+
+    # UnB-HW to R-HW
+    if (sp_cell_type == 3 and ep_cell_type == 4) or (sp_cell_type == 4 and ep_cell_type == 3):
+        # Diag Check
+        if (start_point[0] + 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] - 1 == end_point[1]):
+            return ((math.sqrt(2) + math.sqrt(8)) / 2) / 4
+        # V & H Check
+        elif (start_point[0] == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] == end_point[1]):
+            return 1.5 / 4
+
+    # UnB to UnB-HW
+    if (sp_cell_type == 1 and ep_cell_type == 3) or (sp_cell_type == 3 and ep_cell_type == 1):
+        # Diag Check
+        if (start_point[0] + 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] - 1 == end_point[1]):
+            return (math.sqrt(2) + (math.sqrt(2) / 4)) / 2
+        # V & H Check
+        elif (start_point[0] == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] == end_point[1]):
+            return (1 + (1 / 4)) / 2
+
+    # UnB to R-HW
+    if (sp_cell_type == 1 and ep_cell_type == 4) or (sp_cell_type == 4 and ep_cell_type == 1):
+        # Diag Check
+        if (start_point[0] + 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] - 1 == end_point[1]):
+            return (math.sqrt(2) + (math.sqrt(8) / 4)) / 2
+        # V & H Check
+        elif (start_point[0] == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] == end_point[1]):
+            return (1 + (2 / 4)) / 2
+
+    # R to UnB-HW
+    if (sp_cell_type == 2 and ep_cell_type == 3) or (sp_cell_type == 3 and ep_cell_type == 2):
+        # Diag Check
+        if (start_point[0] + 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] - 1 == end_point[1]):
+            return (math.sqrt(8) + (math.sqrt(2) / 4)) / 2
+        # V & H Check
+        elif (start_point[0] == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] == end_point[1]):
+            return (2 + (1 / 4)) / 2
+
+    # R to R-HW
+    if (sp_cell_type == 2 and ep_cell_type == 4) or (sp_cell_type == 4 and ep_cell_type == 2):
+        # Diag Check
+        if (start_point[0] + 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] - 1 == end_point[1]):
+            return (math.sqrt(8) + (math.sqrt(8) / 4)) / 2
+        # V & H Check
+        elif (start_point[0] == end_point[0] and start_point[1] + 1 == end_point[1]) or\
+                (start_point[0] == end_point[0] and start_point[1] - 1 == end_point[1]) or\
+                (start_point[0] + 1 == end_point[0] and start_point[1] == end_point[1]) or\
+                (start_point[0] - 1 == end_point[0] and start_point[1] == end_point[1]):
+            return (2 + (2 / 4)) / 2
+
+
 def gen_map(centers, highway_paths, block_cells):
     # Creates 2-D array size 160x120 and sets all the values to 1 - Unblocked Cells
-    a = np.empty((160, 120))
+    a = np.empty((120, 160))
     a.fill(1)
 
+    # Generates rough terrain
     for point_set in centers:
         x_value = point_set[0]
         y_value = point_set[1]
 
         left_x_bound = 0
-        right_x_bound = 160
+        right_x_bound = 119
         upper_y_bound = 0
-        lower_y_bound = 120
+        lower_y_bound = 159
 
         if x_value - 15 > 0:
             left_x_bound = x_value - 15
-        if x_value + 15 < 160:
+        if x_value + 15 < 120:
             right_x_bound = x_value + 15
         if y_value - 15 > 0:
             upper_y_bound = y_value - 15
-        if y_value + 15 < 120:
+        if y_value + 15 < 160:
             lower_y_bound = y_value + 15
 
         for y_point in range(upper_y_bound, lower_y_bound):
@@ -173,6 +351,7 @@ def gen_map(centers, highway_paths, block_cells):
                 elif coin == 1:
                     a.itemset((x_point, y_point), 1)
 
+    # Generates highways
     for i in highway_paths:
         for k in i:
             x_val = k[0]
@@ -182,18 +361,35 @@ def gen_map(centers, highway_paths, block_cells):
             elif a.item(x_val, y_val) == 2:
                 a.itemset((x_val, y_val), 4)
 
+    # Generates blocked cells
     for p in block_cells:
         x_val = p[0]
         y_val = p[1]
         a.itemset((x_val, y_val), 0)
 
-    plt.imshow(a, cmap='Paired', interpolation='nearest')
-    plt.show()
+    return a
+
 
 
 # Main
-rough_terrain_centers = ter_center()
-highways = highway_gen()
-blocked_cells = block_cell_gen()
 
-gen_map(rough_terrain_centers, highways, blocked_cells)
+
+menu = True
+while (menu):
+    print("Please make a selection: ")
+    selection = input()
+    if selection == "g" or selection == "G":
+        rough_terrain_centers = ter_center()
+        highways = highway_gen()
+        blocked_cells = block_cell_gen()
+        curr_map = gen_map(rough_terrain_centers, highways, blocked_cells)
+        print("New Map Generated!")
+    elif selection == "show" or selection == "sh":
+        plt.imshow(curr_map, cmap='Paired', interpolation='nearest')
+        plt.show()
+    elif selection == "save" or selection == "s":
+        print("Please enter file name (.txt):")
+        f_name = input()
+        save_map(curr_map, [1, 2], [1, 1], rough_terrain_centers, f_name + ".txt")
+    elif selection == "end" or selection == "e":
+        menu = False
